8ddbb0c77df86d8d83b67b9de61f1d6a69810fbf
Preshh0 <uniquep201@gmail.com>
2021-05-12 01:32:55 +0100

    Extra Random additions.

--- QuickRecap.py
+++ QuickRecap.py
@@ -1,7 +1,12 @@
 #(>>>) stands for Read-Evaluate-Print-Loop
 print ("Hello World")
+'''Variables'''
+
 Greetings = "Hey, How are you?"
 print(Greetings)
+
+'''Operators'''
+
 sum = 2 + 2
 print(sum)
 subtraction = 4-2
@@ -12,3 +17,24 @@ modulus = 3%3
 print(modulus)
 multiplication = 2*3
 print(multiplication)
+exponentiation = 3**4
+print("Exponentiation:",exponentiation)
+Floor = 3//3
+print(Floor)
+#- as a unary operator
+print(-4)
+
+'''Comparison'''
+a = 4
+b = 8
+
+print(a == b)
+print (a != b)
+print(a > b)
+print( a<= b)
+age = 1
+print(type(age) == int)
+#Converting from float to int
+fraction = 0.1
+intFraction = int(fraction)
+print(intFraction)
