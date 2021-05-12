6835ffd2c9b6b1263f6976de24ff2b6480658157
Preshh0 <uniquep201@gmail.com>
2021-05-12 01:03:16 +0100

    Random additions.

---
+++ QuickRecap.py
@@ -0,0 +1,14 @@
+#(>>>) stands for Read-Evaluate-Print-Loop
+print ("Hello World")
+Greetings = "Hey, How are you?"
+print(Greetings)
+sum = 2 + 2
+print(sum)
+subtraction = 4-2
+print(subtraction)
+division = 3/4
+print(division)
+modulus = 3%3
+print(modulus)
+multiplication = 2*3
+print(multiplication)
